8f340119b52a670cfca1d87b231be7a943ae5bb5
Nick Capps <nickcapps10@gmail.com>
2020-03-01 17:39:05 -0800

    Add styled-components and setup stylelint

--- carousel/.eslintrc.js
+++ carousel/.eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ['airbnb', 'plugin:jest/recommended'],
   rules: {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/prop-types"​: ​"off"​,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
   }
 };
--- carousel/src/CarouselSlide.js
+++ carousel/src/CarouselSlide.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Img = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: ${({ imgHeight }) => (typeof imgHeight === 'number'
+    ? `${imgHeight}px`
+    : imgHeight)};
+`;
 
 const CarouselSlide = ({
-  imgUrl, description, attribution, ...rest
+  imgUrl, imgHeight, description, attribution, ...rest
 }) => (
   <figure {...rest}>
-    <img src={imgUrl} />
+    <Img src={imgUrl} imgHeight={imgHeight} />
     <figcaption>
       <strong>{description}</strong>
       {' '}
@@ -15,9 +24,14 @@ const CarouselSlide = ({
 );
 
 CarouselSlide.propTypes = {
+  imgHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   imgUrl: PropTypes.string.isRequired,
   description: PropTypes.node.isRequired,
   attribution: PropTypes.node,
+};
+
+CarouselSlide.defaultProps = {
+  imgHeight: 500,
 };
 
 export default CarouselSlide;
